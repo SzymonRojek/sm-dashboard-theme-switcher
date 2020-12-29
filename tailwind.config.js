1e1f5e06a9c50ed5c04e83b9d79a41fe98fa3885
Szymon Rojek <sz.rojek@gmail.com>
2020-12-29 13:48:41 +0000

    tailwind and gulp config, add styles to the header

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.html'],
+  darkMode: 'class', // 'media' or true
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          bell: '#8C98C6',
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
