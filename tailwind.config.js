5678a5db63dffe6eb3865a88385b1182ce796ecb
Szymon Rojek <sz.rojek@gmail.com>
2020-12-30 22:10:47 +0000

    add new icons social media, customize colors in tailwind.config.js, set new colors for background, font-colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,32 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: {
-          bell: '#8C98C6',
-        }
+          transparent: 'transparent',
+          current: 'currentColor',
+          white: {
+            lilac: '#F1F3FA',
+            zircon: '#F7F9FF',
+          },
+          gray: {
+            alto: '#D8D8D8'
+          },
+          blue: {
+            bell: '#8C98C6',
+            odgerLight: '#1DA1F2',
+            dodgerDark: '#178FF5',
+          },
+          navyDark: {
+            primary: '#20222F',
+            secondary: '#252B42',
+          },
+          green: {
+            mountainMeadow: '#1EB589',
+          },
+          red: {
+            monza: '#C4032B',
+            valencia: '#DC414C',
+          },
       }
     }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  }
 }
