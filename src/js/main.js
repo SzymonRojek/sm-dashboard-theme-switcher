29765a164e4444db4f3386319b1aed2a0fe5eded
Szymon Rojek <sz.rojek@gmail.com>
2021-01-02 18:17:32 +0000

    fix dark mode  js interaction with the button

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,58 @@
-//main.js file
+// // //main.js file
 
-const switcher = document.getElementById('switchTheme');
-const changeTextMode = document.getElementById('phrase');
 
-switcher.addEventListener('click', function() {
-  let htmlClasses = document.querySelector('html').classList;
+let darkMode = localStorage.getItem('dark');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const htmlClasses = document.querySelector('html').classList;
 
-    if(localStorage.theme === 'dark') {
-      htmlClasses.remove('dark');
-      localStorage.removeItem('theme');
-      changeTextMode.textContent = 'dark';
+const textMode = document.getElementById('phrase');
+
+const enableDarkMode = () => {
+  htmlClasses.add('dark');
+  localStorage.setItem('dark', 'enabled');
+
+  textMode.textContent = 'dark';
+}
+
+const disableDarkMode = () => {
+  htmlClasses.remove('dark');
+  localStorage.setItem('dark', null);
+
+  textMode.textContent = 'light';
+}
+
+// check the mode when the page is reload
+
+if (darkMode === 'enabled') {
+  enableDarkMode();
+} else {
+  disableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('dark');
+    if (darkMode !== 'enabled') {
+      enableDarkMode();
     } else {
-      htmlClasses.add('dark'); 
-      localStorage.theme = 'dark';
-      changeTextMode.textContent = 'light';
+      disableDarkMode();
     }
-});
+})
 
 
 
+// // const switcher = document.getElementById('switchTheme');
+// // const changeTextMode = document.getElementById('phrase');
 
-// changeTextMode.addEventListener('load', function() {
-//   if(localStorage.theme === 'dark') {
-//     changeTextMode.textContent = 'dark';
-//   } else {
-//     changeTextMode.textContent = 'light';
-//   }
-//   changeTextMode.removeEventListener('load', load);
-// })
+// // switcher.addEventListener('click', function() {
+// //   let htmlClasses = document.querySelector('html').classList;
+
+// //     if(localStorage.theme === 'dark') {
+// //       htmlClasses.remove('dark');
+// //       localStorage.removeItem('theme');
+// //       changeTextMode.textContent = 'light';
+// //     } else {
+// //       htmlClasses.add('dark'); 
+// //       localStorage.theme = 'dark';
+// //       changeTextMode.textContent = 'dark';
+// //     }
+// // });
