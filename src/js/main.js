dbe655eda42c102829f8a8cfcc2973f75430696a
Szymon Rojek <sz.rojek@gmail.com>
2021-01-05 21:29:58 +0000

    remove img in the button, change an idea of the switch button, add hover, focus

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
-// main.js file
 
 
 let darkMode = localStorage.getItem('bcolor');
 
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const darkModeToggle = document.getElementById('dark-mode-toggle');
 const htmlClasses = document.querySelector('html').classList;
 const textMode = document.getElementById('phrase');
 
@@ -34,7 +33,7 @@ if (darkMode === 'enabledDark') {
 }
 
 darkModeToggle.addEventListener('click', () => {
- 
+ console.log('test');
   darkMode = localStorage.getItem('bcolor');
     if (darkMode !== 'enabledDark') {
       enableDarkMode();
@@ -44,23 +43,3 @@ darkModeToggle.addEventListener('click', () => {
 })
 
 
-
-
-// const switcher = document.getElementById('#dark-mode-toggle');
-// const textMode = document.getElementById('phrase');
-
-// switcher.addEventListener('click', function() {
-//   let htmlClasses = document.querySelector('html').classList;
-
-//     if(localStorage.theme === 'dark') {
-//       htmlClasses.remove('dark');
-//       localStorage.removeItem('theme');
-//       // textMode.textContent = 'light';
-//     } else {
-//       htmlClasses.add('dark'); 
-//       localStorage.theme = 'dark';
-//       // textMode.textContent = 'dark';
-//     }
-// });
-
-require('remove-focus-outline')
