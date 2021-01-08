71e2108cae2dfd4b24e27e77b8a4a8284c73ada9
Szymon Rojek <sz.rojek@gmail.com>
2021-01-08 02:58:00 +0000

    task: it is important tu recognize what theme user prefers - dark or light, try to figure it out

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,21 @@
 
-
 let darkMode = localStorage.getItem('theme');
 const switcherMode = document.getElementById('switcher-mode');
 const htmlClasses = document.querySelector('html').classList;
 const textMode = document.querySelector('.phrase');
+
+function checkDarkMode() {
+  if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+    
+  ) {
+    return true;
+  }
+  return false;
+}
+
+
 
 const enableDarkMode = () => {
   htmlClasses.add('dark');
@@ -15,9 +27,14 @@ const enableDarkMode = () => {
 
 const disableDarkMode = () => {
   htmlClasses.remove('dark');
-  
   localStorage.setItem('theme', null); 
   textMode.textContent = 'dark';
+}
+
+if (checkDarkMode()) {
+  enableDarkMode();
+} else {
+  disableDarkMode();
 }
 
 
@@ -42,14 +59,36 @@ switcherMode.addEventListener('click', ( event ) => {
   handleBtnClick( event );
 }, false)
 
-const afterLoadPage = () => {
-  if (darkMode === 'dark-light') {
-    switcherMode.setAttribute("aria-pressed", true);
-    enableDarkMode()
-  }  else {
-    switcherMode.setAttribute("aria-pressed", false);
-    disableDarkMode();
-  }
-}
-afterLoadPage();
+
+
+
+// celem główbym jest to, by skrypt rozpoznał preferencje uzytkownia do theme - jezeli uzytkownik nie ma nic przeciwko by uzywac dark mode, to ma być ono uznane za docelowe. Jednakze ciekawi mnie taka sytuacja: jezeli user w danym momencie wybierze light mode, to kolejnym celem jest to, by po odswiezeniu strony owo light mode zostawało :) na pewno mozna taki skrypt napisac. 
+
+// aria pressed ma odczytywac status buttona. Jezeli przegladrka wybierze glowne theme to aria pressed dla niego bedzie poczatkowo false. Dlatego jezeli bedzie dark mode to aria-pressed = false, nacisniecie buttona przypisze true do arii, tj. wlaczenie buttona. Teraz istotne jest to, by po odswiezeniu strony aria sie nie zmienila.
+
+// ponizej mam dwie nieudane proby napisania funkcji w kontekscie odswiezania strony: obie sa do kitu, ale cos z nich moze sie przyda...
+
+
+
+// const afterLoadPage = () => {
+//   if (darkMode === 'dark-light') {
+//     switcherMode.setAttribute("aria-pressed", true);
+//     enableDarkMode()
+//   }  else {
+//     switcherMode.setAttribute("aria-pressed", false);
+//     disableDarkMode();
+//   }
+// }
+// afterLoadPage();
+
+
+// const afterLoadPage = () => {
+// if (localStorage.theme === 'dark-light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+//   enableDarkMode()
+// } else {
+//   disableDarkMode();
+// }
+
+// }
+// afterLoadPage();
 
