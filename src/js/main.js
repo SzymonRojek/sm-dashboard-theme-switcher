68f931a8c8e405ad182c3bb0a9521bbd43743827
Szymon Rojek <sz.rojek@gmail.com>
2021-01-12 17:29:38 +0000

    refactoring js - remove unnecessary code

--- src/js/main.js
+++ src/js/main.js
@@ -1,90 +1,46 @@
-
-let darkMode = localStorage.getItem( 'theme' );
 const switcherMode = document.getElementById( 'switcher-mode' );
 const htmlClasses = document.querySelector( 'html' ).classList;
 const textMode = document.querySelector( '.phrase' );
 
-function checkDarkMode() {
-  if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
-    return true;
-  }
-  return false;
-}
-
-const enableDarkMode = () => {
-  
-
-  // element o kluczu bcolor przechować ma wartość enabledDark
-  localStorage.setItem( 'theme', 'dark-light' ); 
+function switchOn() {
   htmlClasses.add( 'dark' );
+  switcherMode.setAttribute( 'aria-checked', true );
   textMode.textContent = 'light';
+  localStorage.setItem( 'theme', 'dark' );
 }
 
-const disableDarkMode = () => {
-
-  localStorage.setItem( 'theme', null ); 
+function switchOff() {
   htmlClasses.remove( 'dark' );
+  switcherMode.setAttribute( 'aria-checked', false );
   textMode.textContent = 'dark';
+  localStorage.removeItem( 'theme' );
 }
 
-if ( checkDarkMode() ) {
-  enableDarkMode();
-} else {
-  disableDarkMode();
-}
-
-
-const handleBtnClick = event => {
-  toggleButton( event.target );
+function setPreferentialTheme() {
+  if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
+    switchOn();
+  } else {
+    switchOff();
+  }
 }
 
 const toggleButton = element => {
-  darkMode = localStorage.getItem( 'theme' );
-
-  let pressed = element.getAttribute( 'aria-pressed' ) === 'true';
-  element.setAttribute( 'aria-pressed', !pressed );
-  
-    if( !pressed && darkMode !== 'dark-light' ) {
-      enableDarkMode();
+    if( localStorage.getItem( 'theme' ) === 'dark') {
+      switchOff();
     } else {
-      disableDarkMode();
+      switchOn();
     }
 }
 
 switcherMode.addEventListener( 'click', ( event ) => {
-  handleBtnClick( event );
+  toggleButton( event.target );
 }, false)
 
 
+// reload the page
+  if (localStorage.getItem( 'theme' ) === 'dark') {
+    switchOn()
+  } else {
+   switchOff()
+  }
 
-/*
-celem głównym jest to, by skrypt rozpoznał preferencje usera do rodzaju theme - 
-jezeli user nie ma nic przeciwko by uzywac dark mode, to ma być ono uznane za docelowe. 
-Jednakze ciekawi mnie taka sytuacja: jezeli user w danym momencie wybierze light mode, 
-to kolejnym celem jest to, by po odswiezeniu strony owo light mode zostawało.
-
-aria pressed ma odczytywac status buttona. 
-Jezeli przegladrka wybierze theme dark lub light w zaleznosci od preferencji
-usera to aria pressed dla niego bedzie poczatkowo false. 
-Dlatego jezeli bedzie dark mode to aria-pressed = false, natomiast nacisniecie buttona przypisze true do arii, co oznacza wlaczenie buttona. T
-eraz istotne jest to, by po odswiezeniu strony aria sie nie zmienila.
-
-Problem double click pozostaje.
-*/
-
-
- const afterReloadPage = () => {
-      if (darkMode === 'dark-light') {
-        enableDarkMode()
-      } else {
-        disableDarkMode();
-      }
-    }
-afterReloadPage();
-
-
-// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-//   document.querySelector('html').classList.add('dark')
-// } else {
-//   document.querySelector('html').classList.remove('dark')
-// }
