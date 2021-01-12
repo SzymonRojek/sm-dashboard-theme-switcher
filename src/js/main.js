948dd4373574e3563fba5d9d7272af3680d41515
Szymon Rojek <sz.rojek@gmail.com>
2021-01-12 17:59:50 +0000

    edited js file

--- src/js/main.js
+++ src/js/main.js
@@ -2,54 +2,54 @@
 (() => {
   'use strict'
 
-const switcherMode = document.getElementById( 'switcher-mode' );
-const htmlClasses = document.querySelector( 'html' ).classList;
-const textMode = document.querySelector( '.phrase' );
+  const switcherMode = document.getElementById( 'switcher-mode' );
+  const htmlClasses = document.querySelector( 'html' ).classList;
+  const textMode = document.querySelector( '.phrase' );
 
 
-function switchOn() {
-  htmlClasses.add( 'dark' );
-  switcherMode.setAttribute( 'aria-checked', true );
-  textMode.textContent = 'light';
-  localStorage.setItem( 'theme', 'dark' );
-}
-
-function switchOff() {
-  htmlClasses.remove( 'dark' );
-  switcherMode.setAttribute( 'aria-checked', false );
-  textMode.textContent = 'dark';
-  localStorage.removeItem( 'theme' );
-}
-
-function setPreferentialTheme() {
-  if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
-    switchOn();
-  } else {
-    switchOff();
+  function switchOn() {
+    htmlClasses.add( 'dark' );
+    switcherMode.setAttribute( 'aria-checked', true );
+    textMode.textContent = 'light';
+    localStorage.setItem( 'theme', 'dark' );
   }
-}
 
-const toggleButton = element => {
-    if( localStorage.getItem( 'theme' ) === 'dark') {
-      switchOff();
-    } else {
+  function switchOff() {
+    htmlClasses.remove( 'dark' );
+    switcherMode.setAttribute( 'aria-checked', false );
+    textMode.textContent = 'dark';
+    localStorage.removeItem( 'theme' );
+  }
+
+  function setPreferentialTheme() {
+    if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
       switchOn();
+    } else {
+      switchOff();
     }
-}
+  }
 
-switcherMode.addEventListener( 'click', ( event ) => {
-  toggleButton( event.target );
-}, false)
+  const toggleButton = element => {
+      if( localStorage.getItem( 'theme' ) === 'dark') {
+        switchOff();
+      } else {
+        switchOn();
+      }
+  }
+
+  switcherMode.addEventListener( 'click', ( event ) => {
+    toggleButton( event.target );
+  }, false)
 
 
-// reload the page
+// keep the choosen theme when the page has been reloaded:
   if (localStorage.getItem( 'theme' ) === 'dark') {
     switchOn();
   } else {
    switchOff();
   }
 
-})()
+})();
 
 
 
